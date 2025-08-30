08f575800297342b2c04b1a54aabafc40f85f1f4
tracyshen2678 <tracyshen301@gmail.com>
2025-08-30 17:51:53 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(title="Grant Application Extractor API")
 # 2. 定义允许的源
 origins = [
     "https://grant-extractor-frontend.onrender.com",
-    "http://localhost:63947", 
+    "http://localhost:49570", 
 ]
 
 app.add_middleware(
